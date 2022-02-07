dad5dd9dc5005d08f623d456665b1bc9d7b33828
pratiknapit <63886155+pratiknapit@users.noreply.github.com>
2022-02-07 23:36:08 +1100

    first commit

--- api/api.py
+++ api/api.py
@@ -1,11 +1,11 @@
 from flask import Flask 
 import time 
-
+from datetime import date
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    return {'time': date.today()}
 
     
